5dc94a438c0c5c1bfb193007ddbe98347cba0e7f
dbonafin <demianbonafina@gmail.com>
2022-07-06 15:55:30 +0200

    page header component done

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div id="app">
-    <p>reset</p>
+
+    <header>
+      <PageHeader/>
+    </header>
+ 
   </div>
 </template>
 
 <script>
+import PageHeader from './components/PageHeader.vue';"./components/PageHeader.vue";
 
 export default {
   name: 'App',
   components: {
+    PageHeader
   }
 }
 </script>
 
 <style lang="scss">
 @import "./style/common.scss";
+
+header {
+  background-color: white;
+}
 </style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,123 @@
+<template>
+    <header class="header container">
+
+        <!-- Logo container -->
+        <div>
+            <img src="../assets/img/dc-logo.png" alt="dc comics logo">
+        </div>
+
+        <!-- Header navigation area -->
+        <nav>
+            <ul>
+                <!-- Create a link for every object in the linksArray - different urls and texts -->
+                <li 
+                v-for="(link, index) in linksArray" 
+                :key="index" 
+                :class="{'active': link.active}"
+                >
+                    <!-- If the item is active - add class 'active' to apply the css styles (li and a tags) -->
+                    <a :class="{'active': link.active}" :href="link.url"> {{link.text}} </a>
+                </li>
+            </ul>
+        </nav>
+
+    </header>
+</template>
+
+<script>
+export default {
+    name: "PageHeader", 
+     data() {
+        return {
+            linksArray: [
+                {
+                    url: "#",
+                    text: "CHARACTERS",
+                    active: false
+                },
+                {
+                    url: "#",
+                    text: "COMICS",
+                    active: true
+                },
+                 {
+                    url: "#",
+                    text: "MOVIES",
+                    active: false
+                },
+                {
+                    url: "#",
+                    text: "TV",
+                    active: false
+                },
+                {
+                    url: "#",
+                    text: "GAMES",
+                    active: false
+                },
+                {
+                    url: "#",
+                    text: "COLLECTIBLES",
+                    active: false
+                }, 
+                 {
+                    url: "#",
+                    text: "VIDEOS",
+                    active: false
+                },
+                {
+                    url: "#",
+                    text: "FANS",
+                    active: false
+                }, 
+                 {
+                    url: "#",
+                    text: "NEWS",
+                    active: false
+                },
+                {
+                    url: "#",
+                    text: "SHOP",
+                    active: false
+                }, 
+
+
+                
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../style/common.scss";
+@import "../style/brand-colors.scss";
+
+header {
+    height: 150px;
+    line-height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    ul {
+        display: flex;
+        li {
+            line-height: 150px;
+            margin: 0 10px;
+            &.active {
+                color: $primary-color;
+                border-bottom: 3px solid $primary-color;
+            } 
+            a {
+                color: $tertiary-color;  
+                font-weight: 700;
+                &.active {
+                color: $primary-color;
+            } 
+            }
+        }
+    }
+}
+
+</style>
